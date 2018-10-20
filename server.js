9fe50fa5c3556ccd996a037e9797da2f7b369692
Eduardo Monforte <eduardomonforte@gmail.com>
2018-10-20 10:45:53 -0500

    Minor text formatting.

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(cors());
-app.use( bodyParser.json() );
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
 app.post('/buscadestino', (req, res) => {
-    let reqData =  req.body;
+    let reqData = req.body;
     let url = "http://gaia.inegi.org.mx/sakbe_v3.1/buscadestino?key=rZMkTZuI-ugbU-t1Y5-zCHg-0ZOETYT3r0HG";
     url += "&num=5&type=json";
     url += "&proj=" + reqData.proj;
@@ -23,13 +23,13 @@ app.post('/buscadestino', (req, res) => {
     .then(response => {
         return res.send(response.data.data);
     })
-    .catch( error => {
+    .catch(error => {
         return res.send({error:error});
     });
 });
 
 app.post('/cuota', (req, res) => {
-    let reqData =  req.body;
+    let reqData = req.body;
     let url = "http://gaia.inegi.org.mx/sakbe_v3.1/cuota?key=rZMkTZuI-ugbU-t1Y5-zCHg-0ZOETYT3r0HG&v=1";
     url += "&type=json&proj=" + reqData.proj;
     url += "&dest_i=" + reqData.startTrip;
@@ -39,13 +39,13 @@ app.post('/cuota', (req, res) => {
         console.log(response);
         return res.send(response.data.data);
     })
-    .catch( error => {
+    .catch(error => {
         return res.send({error:error});
     });
 });
 
 app.post('/combustible', (req, res) => {
-    let reqData =  req.body;
+    let reqData = req.body;
     let url = "http://gaia.inegi.org.mx/sakbe_v3.1/combustible?key=rZMkTZuI-ugbU-t1Y5-zCHg-0ZOETYT3r0HG";
     url += "&type=json&proj=" + reqData.proj;
     axios.post(url)
@@ -53,7 +53,7 @@ app.post('/combustible', (req, res) => {
         console.log(response);
         return res.send(response.data.data);
     })
-    .catch( error => {
+    .catch(error => {
         return res.send({error:error});
     });
 });
